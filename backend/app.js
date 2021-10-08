eb991bad87f5c41d926f740f191f14412e3b8580
Rodrigo Bossini <professorbossini@gmail.com>
2021-10-08 11:39:39 -0300

    Terminamos a secao 2.7 da apostila 04. OBS: É preciso criar um arquivo .env na raiz do projeto e incluir as variaveis MONGODB_USER, MONGODB_PASSWORD e MONGODB_DATABASE

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,15 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require ('cors')
+const Cliente = require ('./models/cliente')
+const mongoose = require ('mongoose')
 const app = express();
 app.use(express.json())
 app.use(cors())
+
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.ssm0w.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`)
+.then(() =>  console.log ("CONEXAO OK"))
+.catch (e => console.log ("CONEXAO NOK: " + e))
 
 const clientes = [
   {
@@ -29,8 +36,13 @@ app.get('/api/clientes', (req, res) => {
 
 //POST localhost:3000/api/clientes
 app.post('/api/clientes', (req, res) => {
-  const cliente = req.body
+  const cliente = new Cliente({
+    nome: req.body.nome,
+    fone: req.body.fone,
+    email: req.body.email
+  })
+  cliente.save()
   console.log(cliente)
-  res.status(201).json({mensagem: 'Cliente inserido'})
+  res.status(201).json({mensagem: "Cliente inserido"})
 })
 module.exports = app;
